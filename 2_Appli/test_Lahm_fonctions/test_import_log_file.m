0dff474888fc557c4961846c065fabd9d55b02eb
Lahm96 <gtouze96@gmail.com>
2019-02-14 14:15:32 +0000

    Prise en compte des temps GPS

--- 2_Appli/test_Lahm_fonctions/test_import_log_file.m
+++ 2_Appli/test_Lahm_fonctions/test_import_log_file.m
@@ -2,64 +2,54 @@
 
 clear
 
-format shortg
-c = clock;
-c(end);
+% format shortg
+% c = clock;
+% c(end);
 
-data_raw  = dlmread_empty('rc_1.log',',',1,0,NaN);
+% time(ms):1  Ax(g):9  Ay(g):10  Az(g):11  Yaw(°/s):12  Pitch(°/s):13  Roll(°/s):14  Lattitude(°):32
+% longitude(°):33  speed(mph):34  height(ft):35  distance(ft):39
+
+raw  = dlmread_empty('rc_0.log',',',1,0,NaN);
 
 data = [];
-for i= 1:length(data_raw)
-    if abs(data_raw(i,11)) >= 0
-        data = [data; data_raw(i,:)];
+for i= 1:length(raw)
+    if abs(raw(i,11)) >= 0
+        data = [data; raw(i,:)];
     end
 end
 
-% R = cellfun(@(x) ) 
+% clear data_raw
 
-clear data_raw
-
-data_gps = []; % [time lattitude longitude speed height distance]
-for i=1:length(data)
-    if abs(data(i,32)) > 0
-        GPS = [data(i,1) data(i, 32:35) data(i, 39)];
+indic = 1;
+data_gps = []; % [time:1 lattitude:2 longitude:3 speed:4 height:5 distance:6 Ax:7 Ay:8 Az:9 Yaw:10 Pitch:11 Roll:12]
+for i=1:length(raw)
+    if abs(raw(i,32)) > 0
+        if raw(i,1) == data(indic,1)
+            A = data(indic,9:14);
+        else
+           while raw(i,1) > data(indic,1) % && data_raw(i,1) > data(indic+1,1)
+               indic = indic + 1;
+           end
+           a = (data(indic,9:14) - data(indic-1,9:14))./(data(indic,1) - data(indic-1,1));
+           b = data(indic,9:14) - a*data(indic,1);
+           A = a*raw(i,1) + b;
+        end
+        GPS = [raw(i,1) raw(i, 32:35) raw(i, 39) A];
         data_gps = [data_gps; GPS];
     end
 end
 data = [data(:,1:31) data(:,40:end)];
 
-b = clock;
-b(end)-c(end)
+clear data_raw
 
-% [X, Y] = lat_longi2X_Y(data_gps(:,2), data_gps(:,3));
-% track_plot(X, Y, data_gps(:,4))
+% b = clock;
+% b(end)-c(end)
 
-%% test cellfun
+%%
 
-a = [[1 2 3]; [4 5 6]; [7 8 9]];
+figure
+plot(data_gps(:,6), data_gps(:,9))
 
-b = num2cell(a);
-
-c = cellfun(@(x) x>5, b)
-
-%% test 2
-
-clear
-
-format shortg
-c = clock;
-c(end);
-
-raw  = dlmread_empty('rc_1.log',',',1,0,NaN);
-raw = num2cell(raw);
-raw(cellfun(@(x) ~isempty(x) && isnumeric(x) && isnan(x),raw)) = {''}; 
-            
-% Replace non-numeric cells with 100000
-R = cellfun(@(x) ~isnumeric(x) && ~islogical(x),raw); % Find non-numeric cells
-raw(R) = {100000}; % Replace non-numeric cells
-            
-% Create output variable
-data = reshape([raw{:}],size(raw));
-
-b = clock;
-b(end)-c(end)
+%%
+[X, Y] = lat_longi2X_Y(data_gps(:,2), data_gps(:,3));
+track_plot(X, Y, data_gps(:,7))
