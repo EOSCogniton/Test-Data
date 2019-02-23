999ca81bec82e537abf097ac9c236125188de1ea
Lahm96 <gtouze96@gmail.com>
2019-02-23 20:49:07 +0000

    Modifs borne dans plot_temps_dist et ajout cercle dans Gforce

--- 2_Appli/test_Lahm_fonctions/test_import_log_file.m
+++ 2_Appli/test_Lahm_fonctions/test_import_log_file.m
@@ -8,10 +8,10 @@ c = clock;
 % time(ms):1  Ax(g):9  Ay(g):10  Az(g):11  Yaw(°/s):12  Pitch(°/s):13  Roll(°/s):14  Lattitude(°):32
 % longitude(°):33  speed(mph):34  height(ft):35  distance(ft):39
 
-raw  = dlmread_empty('rc_1.log',',',1,0,NaN);
+raw  = dlmread_empty('rc_0.log',',',1,0,NaN);
 
 time = (raw(:,1)-raw(1,1))*10^-3;
-time(end)
+% time(end)
 
 data = [];
 for i= 1:length(raw)
@@ -48,14 +48,50 @@ clear data_raw
 b = clock;
 b(end)-c(end);
 
-time = (data_gps(:,1)-data_gps(1,1))*10^-3;
-time(end);
+% time = (data_gps(:,1)-data_gps(1,1))*10^-3;
+% time(end);
 
 %%
 
-figure
-plot((data_gps(:,1)-data_gps(1,1))*10^-3, data_gps(:,9))
+% figure
+% plot(abs(data(:,9)), abs(data(:,10)), '+')
 
-% %%
-% [X, Y] = lat_longi2X_Y(data_gps(:,2), data_gps(:,3));
-% track_plot(X, Y, data_gps(:,7))
+% %% test cercle
+% 
+% u = data(:,9);
+% v =  data(:,10);
+% 
+% xs = @(a) sqrt(a(2)^2 / ((a(2)/a(1))^2 + (u ./ v).^2));
+% ys = @(a) v./u .* xs(a)';
+% 
+% % contrainte = @(a) - abs([xs(a)' ; ys(a)]) + abs([u ; v]);
+% contrainte = @(a) [- abs([xs(a)' ; ys(a)]) + abs([u ; v]), []];
+% 
+% % nonlcon = @contrainte;
+% 
+% fonc = @(a) abs(a(1))+abs(a(2));
+% 
+% sol = fmincon(fonc, [1.3 1.5], [], [], [], [], [], [], contrainte);
+
+% function [c,ceq] = circlecon(a)
+% xs = sqrt(a(2)^2 / ((a(2)/a(1))^2 + (u ./ v).^2));
+% ys = v./u .* xs';
+% % c = contrainte(x);
+% c = - abs([xs' ; ys]) + abs([u ; v]);
+% ceq = [];
+% end
+
+%% test autrement
+
+b = max(abs(data(:,10)));
+a = max(abs(data(:,9)));
+
+% x = data(:,9);
+x = (-max(abs(data(:,9))):0.001:max(abs(data(:,9))));
+y = b*sqrt(1 - (x/a).^2);
+
+figure
+hold on
+plot(x,y, 'b')
+plot(x,-y, 'b')
+plot(data(:,9), data(:,10), '*')
