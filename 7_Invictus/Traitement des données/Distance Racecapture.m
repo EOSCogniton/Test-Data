79147c8029a65335ca2e0421bc527a40df50427e
Mathieu Jacquet <jacquet_mathieu@orange.fr>
2021-04-30 02:07:53 +0200

    traitement données carto

--- 7_Invictus/Traitement des données/Distance Racecapture.m
+++ 7_Invictus/Traitement des données/Distance Racecapture.m
@@ -4,12 +4,15 @@ close all;
 clear all;
 
 dist_total_cap = 0;
-filename = sprintf('/Users/bacs/Downloads/7_Invictus/Session roulage Fiabilite - Transpolis 09 04 21/Racecapture/rc_5.log');
-T = readtable(filename); 
+filename = sprintf('/Users/bacs/Documents (non iCloud)/EPSA (non iCloud)/Test-Data/4_Optimus/23_06_19/JMN/rc_1.log');
+T = readtable(filename);
 
 N = height(T);
 
-time = (table2array(T(1:N,1))-8000)./1000;
+time = (table2array(T(1:N,1))-5383)./1000;
+
+%% Distance 
+
 ax = table2array(T(1:N,10));
 ay = table2array(T(1:N,11));
 az = table2array(T(1:N,12));
@@ -23,7 +26,7 @@ dy = cumtrapz(linspace(0,0.04*(N-1),N),vy);
 vz = cumtrapz(linspace(0,0.04*(N-1),N),az);
 dz = cumtrapz(linspace(0,0.04*(N-1),N),vz);
 
-%% Plots
+%% Plots de distance
 
 plot(time,ax,'r');
 hold on;
@@ -53,3 +56,24 @@ plot(time,dz,'b');
 legend("dx","dy","dz");
 ylabel("position (m)");
 xlabel("temps (s)")
+
+%% Freinage
+BF = table2array(T(1:N,3));
+BR = table2array(T(1:N,4));
+Brake_F = [];
+Brake_R = [];
+
+for i = 1:N
+    if mod(i,5) == 0
+        Brake_F(end+1) = BF(i);
+        Brake_R(end+1) = BR(i);
+    end
+end
+
+
+
+    
+    
+    
+    
+    
